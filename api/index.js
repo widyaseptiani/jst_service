063285e020adc4c5dc1b70ae2b54401e9166daa3
widyaseptiani <96729839+widyaseptiani@users.noreply.github.com>
2021-12-27 23:48:04 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1733547356:AAEOX7oG_z09vS34M-DUHOm5YCPsXYDXohg'
+const token = '5065542871:AAGKzMVaR72ebZ-IuoE3rN9zyg0qKQSpfZUg'
 const bot = new TelegramBot(token, {polling: true});
 
 
